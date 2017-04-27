14ba2f59254d776a5003c0e41916ea3f2f5659b8
Ikechi <consumerike@gmail.com>
2017-04-27 17:53:15 -0500

    simple tuple

---
+++ tuple_manipulation.py
@@ -0,0 +1,8 @@
+#the immutable tuple list of simple foods:
+def main():
+    simple_foods =  ('raw fish', 'nuts', 'berries', 'tree bark')
+    for food in simple_foods:
+        print(food)
+
+if __name__ == '__main__':
+    main()
